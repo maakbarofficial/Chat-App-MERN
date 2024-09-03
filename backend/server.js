d4cdf1f63667f6d1c0855afaa271dd02564afdb8
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2024-09-03 19:52:23 +0500

    Add healthcheck route to fix Prod bug of Home Page

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
+app.get("/health", (req, res) => {
   res.json("Chat App Backend Server is running");
 });
 
