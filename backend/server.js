e1870402b1f654be8d5cbbab4206eccd136ba0a6
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2024-09-03 19:31:56 +0500

    Add changes for deployment with API Collection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+
 import connectDB from "./db/connectDB.js";
 import { app, server } from "./socket/socket.js";
+
+const __dirname = path.resolve();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -20,6 +25,12 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectDB();
